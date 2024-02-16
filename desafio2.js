d8e54659552c147391ceb3681101732e0b5c8676
mardefronteira <mmv341@nyu.edu>
2024-02-16 01:43:43 -0300

    Estrutura inicial

---
+++ desafio2.js
@@ -0,0 +1,9 @@
+let desafio2 = () => {
+
+    const tela = document.getElementById("desafio");
+    tela.innerHTML = `
+        <h2>Lógica JS 2/7: 👩🏽‍💻 Variáveis</h2>
+        <p></p>
+        `;
+
+}
---
+++ desafio3.js
@@ -0,0 +1,9 @@
+let desafio3 = () => {
+
+    const tela = document.getElementById("desafio");
+    tela.innerHTML = `
+        <h2>Lógica JS 3/7: Fluxo de decisão</h2>
+        <p></p>
+        `;
+
+}
